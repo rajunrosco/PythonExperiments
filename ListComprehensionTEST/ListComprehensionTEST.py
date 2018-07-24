fc070ac6bc12165803f0aecd4a37be1cea0e903f
Benson Yee <benson_yee@yahoo.com>
2018-07-24 00:16:13 -0600

    Add ListComprehension tests

---
+++ ListComprehensionTEST/ListComprehensionTEST.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+
+def ListComprehensionTests():
+
+    # fill a list in one line
+    list = [x*2 for x in range(0,10)]
+
+    print("hello")
+
+
+def DictComprehensionTests():
+
+    keylist = ["one","two","three","four","five"]
+
+    # fill a dictionary with comprehension
+    dict = { keylist[i]:i+1 for i in range(0,len(keylist))} 
+
+    vallist = [111,222,333,444,555]
+
+    # another way of accomplishing zip() function will return a dictionary instead of zip object
+    dictzip = { keylist[i]:vallist[i] for i in range(0,len(keylist)) } if len(keylist)==len(vallist) else {}
+
+    # zip function creates zip object of tuples that combine 2 lists
+    dictzip2 = zip(keylist, vallist)
+
+    print("hello")    
+
+
+def Main(argv):
+    
+    ListComprehensionTests()
+
+    DictComprehensionTests()
+
+# If module is executed by name using python.exe, enter script through Main() method.  If it is imported as a module, Main() is never executed at it is used as a library
+if __name__ == "__main__":
+    Main(sys.argv)
