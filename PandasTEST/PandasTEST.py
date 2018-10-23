4d1b5d50f76d36551a5cbba6a439c66141ddf7c7
Benson Yee <benson_yee@yahoo.com>
2018-10-23 00:50:48 -0600

    Update PandasTEST

--- PandasTEST/PandasTEST.py
+++ PandasTEST/PandasTEST.py
@@ -4,25 +4,29 @@ import numpy as np
 
 # pandas Series examples 
 def SeriesExample():
-	print("******************************************************")
-	print("* Begin SeriesExample()")
-	print("******************************************************")
-	print()
-	data = np.array(['a','b','c',1.25])
-	s = pd.Series(data)
-	print(s)
-	print()
+    print("******************************************************")
+    print("* Begin SeriesExample()")
+    print("******************************************************")
+    print()
+    data = np.array(['a','b','c',1.25])
+    s = pd.Series(data)
+
+    s2 = pd.Series(['aa','bb','cc'])
+    print("s=\n",s)
+    print()
+    print("s2=\n",s2)
+    print()
 
 
-	s = pd.Series([1,2,3.5,"ben"], index=['A','B','C','D'])
-	print(s)
-	print()
-	print('s[2]={}  type={}'.format(s[2], type(s[2])))
-	print('s["B"]={}  type={}'.format(s["B"], type(s["B"])))
-	print('s["D"]={}  type={}'.format(s["D"], type(s["D"])))
-	print()
-	
-	print()
+    s = pd.Series([1,2,3.5,"ben"], index=['A','B','C','D'])
+    print(s)
+    print()
+    print('s[2]={}  type={}'.format(s[2], type(s[2])))
+    print('s["B"]={}  type={}'.format(s["B"], type(s["B"])))
+    print('s["D"]={}  type={}'.format(s["D"], type(s["D"])))
+    print()
+
+    print()
 
 
 # pandas DataFrame examples
