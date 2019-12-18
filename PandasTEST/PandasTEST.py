211946b82463ea1d67c522f555ed09bb64110a9b
Benson Yee <benson_yee@yahoo.com>
2019-12-17 23:35:04 -0700

    Comments

--- PandasTEST/PandasTEST.py
+++ PandasTEST/PandasTEST.py
@@ -440,10 +440,13 @@ def ReadExcel():
     print()
 
 def ReadCSV():
+    # CSV file "Duration" column are floats with spaces in front of them as well as an empty row. This results
+    # in the DataFrame thinking the column is an object (string) rather than a float
     df = pd.read_csv('TestDataWithHeader.csv')
     printdf("df",df)
     print(df.info())
 
+    # skipinitialspace option removes spaces after delimiter.  Doing this correctly loads column as float with NaN in empty row
     df = pd.read_csv('TestDataWithHeader.csv',skipinitialspace=True)
     printdf("df",df)
     print(df.info())
