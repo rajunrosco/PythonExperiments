897673441280736daac74eeb7c8710ab8a55460f
rajunrosco <benson_yee@yahoo.com>
2020-01-30 15:24:33 -0700

    Remove old experiment

--- pyinstaller_configTEST/pyconfig.py
+++
@@ -1,5 +0,0 @@
-buildpaths = { 	"win64" : {	"Loose" : "\\\\slc-files\\windowsnoeditor\\loose",
-						"Package" : "\\\\slc-files\\windowsnoeditor\\package"},
-				"xboxone" : { "Loose" : "\\\\slc-files\\xboxone\\loose",
-						"Package" : "\\\\slc-files\\xboxone\\package"}
-			}
--- pyinstaller_configTEST/pyexe.py
+++
@@ -1,7 +0,0 @@
-import pyconfig
-
-
-
-
-print (pyconfig.buildpaths["win64"]["Package"])
-print (pyconfig.buildpaths["xboxone"]["Loose"])
