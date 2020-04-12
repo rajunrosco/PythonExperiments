a74264ec0dd8101ae39fdd9a78fa974e0d93574c
Benson Yee <benson_yee@yahoo.com>
2020-04-11 21:40:00 -0600

    Update Recording Status Prototype

--- PandasTEST/Prototype_RecordingStatus.py
+++ PandasTEST/Prototype_RecordingStatus.py
@@ -16,11 +16,33 @@ PandasUtil.printdf("dfLocDirect",dfLocDirect)
 dfUpdate = pd.read_csv('RecordingStatusUpdate.csv')
 PandasUtil.printdf("dfUpdate",dfUpdate)
 
+# Create a new column 'RecordingStatus' that looks at 'common' and 'en-us' columns to determine value
 dfUpdate["RecordingStatus"] = np.where(dfUpdate['en-us'].isnull(), np.where(dfUpdate['common'].isnull(),"Not Recorded",dfUpdate['common']), dfUpdate['en-us'])
 
+# Alternatively, dataframe.apply to axis="columns" will apply a function to each element in the column.  Example function is a lambda, in-line function
+dfUpdate["RecordingStatus2"] = dfUpdate.apply( lambda x: ("Not Recorded!" if x['common'] is np.nan else x['common']) if x['en-us'] is np.nan else x['en-us'], axis='columns' )
+
 # Create a new column 'path' that looks at 'common' and 'en-us' columns to determine value
-dfUpdate["path"] = np.where(dfUpdate['en-us'].isnull(), np.where(dfUpdate['common'].isnull(),"Not Found!",dfUpdate['common']), "Voices\\en-us")
+dfUpdate["path"] = np.where(dfUpdate['en-us'].isnull(), np.where(dfUpdate['common'].isnull(),"Not Found!","Voices\\common"), "Voices\\en-us")
+
+# Alertnatively. dataframe.apple to axis='column' will apply function with one argument to each row.  Single argument function in this case: CurrentDFRow supplies the current Dataframe row to the funtion.  
+def UpdateCurrentRow_RecordingStatus( CurrentDFRow ):
+    if CurrentDFRow['en-us'] is np.nan:
+        if CurrentDFRow['common'] is np.nan:
+            return "Path not Found!"
+        else:
+            return "Voices\\Common\\"
+    else:
+        return "Voices\\en-us\\"
+
+dfUpdate['path2'] = dfUpdate.apply( UpdateCurrentRow_RecordingStatus, axis='columns')
+
+
+
 PandasUtil.printdf("dfUpdate",dfUpdate)
+
+
+
 
 dfLocDirect.set_index('identifierName', drop=False, inplace=True)
 PandasUtil.printdf("dfLocDirect",dfLocDirect)
