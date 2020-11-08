a3dcab4e8833e23b5022b9b0ee783e22efa1a13f
rajunrosco <benson_yee@yahoo.com>
2020-11-07 23:50:05 -0700

    Update DateTimeTest.py

--- DateTimeTest/DateTimeTest.py
+++ DateTimeTest/DateTimeTest.py
@@ -11,16 +11,28 @@ def Practice_Datetime():
 	newdate = today + delay
 	result = newdate.strftime("%A %B %d %Y")
 	if newdate.strftime("%A").lower() == "friday":
-		print "TGIF!"
+		print("TGIF!")
 	
-	print result
-	
-	print "Birthday = "+birthday.strftime("%A, %B, %d, %Y")
-	print "Birthday = "+birthday.strftime("%m/%d/%Y")
+	print(result)
+	print("Birthday = "+birthday.strftime("%A, %B, %d, %Y"))
+	print("Birthday = "+birthday.strftime("%m/%d/%Y"))
 	delta = newdate - birthday
 	
-	print delta.days
-	
+	print(delta.days)
+
+
+	#String to DateTime Object:
+	datetime_str = '09/19/18 13:55:26'
+
+	datetime_object = datetime.datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S')
+
+	print(type(datetime_object))
+	print(datetime_object)  # printed in default format
+
+	weirdtime_str = "A_Weird_20201106_135525"
+	weirdtime_object = datetime.datetime.strptime(weirdtime_str, 'A_Weird_%Y%m%d_%H%M%S')
+	print(weirdtime_object)
+		
 def main():
 	Practice_Datetime()
 	
