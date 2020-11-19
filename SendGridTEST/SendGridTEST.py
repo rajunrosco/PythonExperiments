c68447c26f30d7f447fd547e0b9355ad158e967e
Benson Yee <benson_yee@yahoo.com>
2020-11-18 22:32:53 -0700

    Update SendGridTEST.py

--- SendGridTEST/SendGridTEST.py
+++ SendGridTEST/SendGridTEST.py
@@ -15,11 +15,40 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+HTML_MESSAGE='''
+Dear valued stockist,<br>
+<br>
+To make our delivery service as smooth as possible over the Christmas period please contact me by phone or reply to this email of your planned days of closure to assure we do not attempt to deliver while you are enjoying a well-earned break.<br>
+<br>
+Please include the following information:<br>
+<br>
+	<b>Your Customer Account Number:<br>
+<br>
+	Your Shop Name:<br>
+<br>
+	The last date you can accept a delivery in 2019:<br>
+<br>
+	The first date you can accept a delivery in 2020:</b><br>
+<br>
+<br>
+We will be closed 5:00pm ,Monday, December 23rd 2019 and re-open at 9:30am on Thursday January 2nd 2020.<br>
+<br>
+Kind regards,<br>
+<br>
+<br>
+<br>
+Gareth Tipton, Accounts Assistant UK<br>
+<a href="mailto:gareth.tipton@maggiesottero.com?subject=re:Maggie Sottero Designs LTD Weekly Dispatch Invoice [TEST]">gareth.tipton@maggiesottero.com</a><br>
+Maggie Sottero Designs®<br>
+Tel: 0344 324 0324 (Main)<br>
+Tel: +44 (0)151 482 3000 (International)<br>
+'''
 
 HTML_TEMPLATE = '''
-This is just some plain text beforehand
-
-
+<br>
+This is just some plain text beforehand<br>
+<br>
+<br>
 <table role="table" rules="rows" style="border:1px solid #000000;">
     <thead role="rowgroup">
         <tr role="row">
@@ -145,12 +174,14 @@ HTML_TABLE = '''
 #set API_KEY env var on computer used to send mail
 sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
 
-mymessage = Mail()
-mymessage.from_email = From('phuong.tran@maggiesoterro.com','Phuong T Tran')
-mymessage.to = To('benson_yee@yahoo.com','Benson H Yee')
-mymessage.subject = Subject('Text PDF attachment')
-mymessage.content = Content(MimeType.html, HTML_TEMPLATE)
+#mymessage = Mail()
+myfrom = From('phuong.tran@maggiesoterro.com','Phuong T Tran')
+myto = To('benson_yee@yahoo.com','Benson H Yee')
+mysubject = Subject('Text PDF attachment')
+mycontent1 = Content(MimeType.text, "This is some rando text!")
+mycontent2 = Content(MimeType.html, HTML_MESSAGE+HTML_TEMPLATE)
 
+mymessage = Mail(from_email=myfrom, to_emails=myto, subject=mysubject, plain_text_content=mycontent1, html_content=mycontent2)
 
 fp = open(r'C:\GitHub\DEV_MAGGIE\Source\MaggieMailer\resource\Test01.pdf', 'rb')
 data = fp.read()
