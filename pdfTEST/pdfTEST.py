6fd340e6a7bbeac9d6bb044c4015d00f065b3165
rajunrosco <benson_yee@yahoo.com>
2025-09-18 21:10:38 -0600

    Update pdfTEST.py
    update to python 3.11

--- pdfTEST/pdfTEST.py
+++ pdfTEST/pdfTEST.py
@@ -1,39 +1,55 @@
 # -*- coding: utf-8 -*-
 
-# Personal Python 3.6 Template
-import getopt
+# Personal Python 3.11 Template
+import logging
 import os
-import sys
+import pathlib
 import PyPDF2
+
+_MODULEPATH = pathlib.Path(__file__).parent
+_PDFMERGED = (_MODULEPATH / "pdfMERGED.pdf").resolve()
+
+# Set up logging
+_LOGFILEPATH = (_MODULEPATH / "pdfTEST.log").resolve()
+logging.basicConfig(
+    level=logging.INFO,
+    style="{",
+    format="{asctime} [{levelname}] {message}",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler(_LOGFILEPATH),
+        logging.StreamHandler()
+    ]
+)
+log = logging.getLogger(__name__)
 
 # Get all the PDF filenames.
 pdfFiles = []
-for filename in os.listdir('.'):
-    if filename.endswith('.pdf'):
-        pdfFiles.append(filename)
+for filename in pathlib.Path.glob(_MODULEPATH,"*.pdf"):
+    pdfFiles.append(str(filename))
  
 pdfFiles.sort(key=str.lower)
 
-pdfWriter = PyPDF2.PdfFileWriter()
+pdfWriter = PyPDF2.PdfWriter()
 
 # Loop through all the PDF files.
 for filename in pdfFiles:
-    print("->Merging: {}".format(filename))
+    log.info(f"->Merging: {filename}")
     pdfFileObj = open(filename, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
 
     # Loop through all the pages (except the first) and add them.
-    for pageNum in range(0, pdfReader.numPages):
-        pageObj = pdfReader.getPage(pageNum)
-        pdfWriter.addPage(pageObj)
+    for pageNum in range(0, len(pdfReader.pages)):
+        pageObj = pdfReader.pages[pageNum]
+        pdfWriter.add_page(pageObj)
     pdfFileObj.close()
 
-if os.path.exists('pdfMERGED.pdf'):
-    print("Removing preiously merged pdf")
-    os.remove("pdfMERGED.pdf")
+if os.path.exists(_PDFMERGED):
+    log.info(f"Removing preiously merged pdf: {_PDFMERGED}")
+    os.remove(_PDFMERGED)
 
 # Save the resulting PDF to a file.
-pdfOutput = open('pdfMERGED.pdf', 'wb')
-print("Writing merged pdf:  pdfMERGED.pdf")
+pdfOutput = open(_PDFMERGED, 'wb')
+log.info(f"Writing merged pdf:  {_PDFMERGED}")
 pdfWriter.write(pdfOutput)
 pdfOutput.close()
