9c0fb726e5b5ad394bdcc0b7e9a2b8797c41b377
rajunrosco <benson_yee@yahoo.com>
2026-04-01 01:37:26 -0600

    Update Dataclass.py
    update examples

--- Dataclass/Dataclass.py
+++ Dataclass/Dataclass.py
@@ -8,14 +8,7 @@ import json
 import dataclasses
 from dataclasses import dataclass, field, fields
 
-
-DataClassPetInfo_struct = [("name",str),("sex",str),("dob",datetime.date)]
-
-DataClassPetInfo = dataclasses.make_dataclass("DataClassPetInfo",DataClassPetInfo_struct)
-
-mypet = DataClassPetInfo("Copper","M",datetime.date(2000,1,1))
-
-
+# Creating a frozen dataclass that is immutable and has calculated properties. 
 @dataclass(frozen=True)
 class CMyFrozen:
     formatstring: str
@@ -25,11 +18,20 @@ class CMyFrozen:
         return len(self.formatstring)
 
 Frozen1 = CMyFrozen("Benson")
-
 Frozen2 = CMyFrozen("Cleon")
 
+# NICE!  Dataclasss members can be accessed as properties and they are read only because the dataclass is frozen.  
+# Also, the len property is calculated on the fly and is not stored in the dataclass.  This is a great way to have calculated properties that are read only.
+print(Frozen1.formatstring)
+print(Frozen2.formatstring) 
 print(Frozen1.len)
 print(Frozen2.len)
+# The dataclass also creates a __dict__ for the class that contains the members of the class.  
+# This is a great way to have a dictionary of the members of the class that can be accessed as properties.
+print(getattr(Frozen1,"formatstring"))
+print(getattr(Frozen2,"formatstring"))
+print(getattr(Frozen1,"len"))
+print(getattr(Frozen2,"len"))
 
 try:
     Frozen2.formatstring = "bozo"  # This will generate an error because the dataclass is frozen and acts like a const
@@ -92,17 +94,19 @@ class Address:
 @dataclass
 class Person:
     Name: str = ""
-    Relatives : list[str] = field(default_factory=list) # Datalace constructor for containers and classes
+    Relatives : list[str] = field(default_factory=list[str]) # Dataclass constructor for containers and classes
     HomeAddress: Address = field(default_factory=Address)
 
     @classmethod
     def fieldlist(cls) -> list:
         return [f.name for f in fields(cls)]
     
+    # class method to create an instance of the class from a dictionary.  
+    # This is a great way to create an instance of the class from a JSON object.  
+    # It also filters out any keys that are not in the dataclass, which is a great way to avoid errors when creating an instance of the class from a JSON object that may have extra keys.
     @classmethod
     def from_dict(cls, dict_data: dict):
-        fieldlist = [f.name for f in fields(cls)]
-        filtered_data = {k:v for k,v in dict_data.items() if k in fieldlist}
+        filtered_data = {k:v for k,v in dict_data.items() if k in cls.fieldlist()}
         return cls(**filtered_data)
 
 @dataclass
@@ -136,10 +140,13 @@ json_employees = '''
     }
 ]
 '''
+employee0 = Employee.from_dict(json.loads(json_employees)[0])
+employee1 = Employee.from_dict(json.loads(json_employees)[1])
 
 employee_list = json.loads(json_employees)
 
-
+# Create a lookup class that contains a dictionary of employees by Id and by Name.  
+# This is an alternate way to load the list of employees above into a special dataclass.
 @dataclass
 class EmployeeLookup:
     byId: dict = field(default_factory=dict)
@@ -154,7 +161,6 @@ class EmployeeLookup:
             lookup.byName[emp.Name] = emp
         return lookup
 
-employee_lookup = EmployeeLookup.from_employee_list(employee_list
-                                                    )
+employee_lookup = EmployeeLookup.from_employee_list(employee_list)
 
 print("End experiment")
